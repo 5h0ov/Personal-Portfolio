752b1100753d0067d04458299e029d295acaff4e
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2026-05-18 13:22:30 +0530

    added apollo script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -130,6 +130,14 @@ export default function RootLayout({
           src="https://scripts.simpleanalyticscdn.com/latest.js"
           strategy="afterInteractive"
         />
+        <Script
+          id="apollo-analytics"
+          strategy="afterInteractive"
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+          dangerouslySetInnerHTML={{
+            __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,o.onload=function(){window.trackingFunctions.onLoad({appId:"68e6538d2552450019b83d27"})},document.head.appendChild(o)}initApollo();`,
+          }}
+        />
       </body>
     </html>
   );
